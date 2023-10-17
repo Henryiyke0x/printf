ce410c7f0710294c06a47e1a79559185309d2531
henryiyke0x <spacecannon7@gmail.com>
2023-10-17 14:11:33 -0700

    updated

--- print_char.c
+++ print_char.c
@@ -1,15 +1,118 @@
 #include "main.h"
-/**
- * function to print out a character - print_char.c
- * return 1 on sucess
- */
-int printf_char(va_list var)
-{
-	char str;
+#include <stdlib.h>
 
-	str = va_arg(val, int)
-		_putchar(str)
-		return(1);
+/**
+ * print_c - prints a char
+ * @c: stands for character to it will print,
+ *
+ * Return 1 on success
+ */
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
 }
-/* function to print a char.
+
+/**
+ * print_s - prints out a string
+ * @s: stands for string to printed
+ * Returns the number of characters to be printed
+ */
+
+
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
+
+/**
+ * hex_print - this will print out a character's ascii value in upper case HEX
+ * @c: char to print
+ *
+ * Return: number of chars printed on success ( 2)
+ */
+
+
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
+ * print_S - prints a sting that prints character in ascii values
+ * @S: shows the string to be printed
+ * Return the numbers  of chars printed
+ */
+int print_S(va_list S)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+/* new func */
+
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
+/** 
+ * print_r = t0 print a sring out in reverse 
+ * @r: string to print
+ * return the number of characters
  */
---
+++ print_string.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * function to print string length
+ * return 1 on success
+ */
+int _strlen(char *str)
+
