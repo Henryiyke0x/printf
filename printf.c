b083a796c28f4d50227624497c733d2f47cc1b2f
henryiyke0x <spacecannon7@gmail.com>
2023-10-17 14:28:14 -0700

    updated

---
+++ printf.c
@@ -0,0 +1,87 @@
+ #include "main.h"
+#include <stdlib.h>
+
+/**
+ * check_for_specifiers - this will check whether there is a  format specifier available
+ * @format- this is a format modifier
+ * Return - this returns a  pointer to a function or a NULL value
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+
+	unsigned int i;
+
+	print_t p[] = {
+
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+
+	};
+
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+
+}
+
+/**
+ * _printf - the printf function prints out anything
+ * @format - this list out the numbers of arguments passed to the function
+ * Return- the number of chars that is printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
+}
